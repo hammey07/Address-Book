35e822da6292edaac700240049c127212ee9afff
hammey07 <hammad_saleem@live.com>
2019-10-04 10:07:50 +0100

    Start up

---
+++ src/AddressBook.java
@@ -0,0 +1,5 @@
+public class AddressBook {
+    public static void main(String [] args){
+        
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,47 @@
+public class Person {
+    private String firstname;
+    private String lastname;
+    private String phone;
+    private String address;
+
+    public Person(String firstname, String lastname, String phone, String address) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+}
